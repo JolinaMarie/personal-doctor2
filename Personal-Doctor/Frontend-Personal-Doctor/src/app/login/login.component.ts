dd16f3c42b2c53cac2a2d910a8837a0c8e259348
Kathrin Küster <k.kuester.azubi@ma.owl-it.de>
2023-09-26 11:35:26 +0200

    Login Backend verbindung

--- Personal-Doctor/Frontend-Personal-Doctor/src/app/login/login.component.ts
+++ Personal-Doctor/Frontend-Personal-Doctor/src/app/login/login.component.ts
@@ -1,14 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../model/user.model';
+import { LoginService } from './login.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent{
   hide = true;
-  constructor() {}
+  user: User = new User();
+  errorMessage: string = '';
+  constructor(private loginService: LoginService) { }
 
-  ngOnInit(): void {}
+  loginUser() {
+    if (!this.user.email || !this.user.password) {
+      this.errorMessage = 'Es müssen beide Felder ausgefüllt sein!';
+      return;
+    }
 
+    this.loginService.loginUser(this.user).subscribe(
+      (response: any) => {
+        console.log('User login successful', response);
+      },
+      (error) => {
+        console.error('User login failed', error);
+        this.errorMessage = 'Login failed: ' + error.message;
+
+        // Password Feld wird nach dem Fehlerhaften einloggen leer gemacht
+        this.user.password = '';
+      }
+    );
+  }
 }
